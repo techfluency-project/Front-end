02f6699c68815fb6bdd3dc9c3df713309067a7f0
Artur Coelho <arturcoelho.cps@gmail.com>
2025-05-30 23:37:05 -0300

    sasadadsa

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-module.exports = { 
-  plugins: [ 
-    'postcss-import', 
-    'tailwindcss/nesting', 
-    'tailwindcss', 
-    'autoprefixer', 
-  ] 
-}
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+export default config;
